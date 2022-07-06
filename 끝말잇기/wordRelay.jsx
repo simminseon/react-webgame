d609fedbc34a188c8dde149c19fde87288c74f34
simminseon <alstjs7440@naver.com>
2022-07-06 18:07:39 +0900

    끝말잇기 webpack 설정

---
+++ 끝말잇기/wordRelay.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+const { Component } = React;
+
+function WordRelay() {
+    return (
+        <>
+            <div>test</div>
+        </>
+    ); 
+}
+
+module.exports = WordRelay;
