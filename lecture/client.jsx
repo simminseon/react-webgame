6f814c1902dd1908d1dc9177b9bb021783b390e8
simminseon <alstjs7440@naver.com>
2022-07-20 09:49:41 +0900

    폴더 전체 삭제

--- lecture/client.jsx
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-
-const WordRelay = require('./wordRelay');
-
-ReactDom.render(<WordRelay />, document.querySelector('#root'));
--- 숫자야구/try.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Try() {
-    return (
-        <li>test</li>
-    );
-}
-
-export default Try;
