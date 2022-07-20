cdf001ee4f89cf2f99a788b8cc5d9e8b6a07e57e
simminseon <alstjs7440@naver.com>
2022-07-20 18:06:06 +0900

    끝말잇기 중간 업로드

--- src/pages/wordRelay/WordRelay.jsx
+++ src/pages/wordRelay/WordRelay.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 function WordRelay() {
-  return <div>끝말잇기</div>;
+  const [peopleNum, setPeopleNum] = React.useState("");
+  const [start, setStart] = React.useState(false);
+  const [order, setOrder] = React.useState(1);
+  const [word, setWord] = React.useState("");
+  const [value, setValue] = React.useState("");
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+    setPeopleNum(e.target.value);
+  };
+
+  const onClickButton = () => {
+    console.log("test");
+  };
+
+  const onClickStart = () => {
+    setStart(true);
+  };
+
+  return (
+    <>
+      {start ? (
+        <div id="game">
+          <div>{order}번째 참가자</div>
+          <div>제시어: {word}</div>
+          <input type="text" id="wordInput" onChange={onChangeInput} value={value} />
+          <button type="button" onClick={onClickButton}>
+            입력
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div>몇명이 참가하나요?</div>
+          <input type="text" onChange={onChangeInput} value={peopleNum} />
+          <button type="button" onClick={onClickStart}>
+            시작
+          </button>
+        </div>
+      )}
+    </>
+  );
+
+  // return (
+  //   <>
+
+  //
+  //   </>
+  // );
 }
 
 export default WordRelay;
