524b116f6f8e33933839dfc87d6b9502e3a3b95f
simminseon <alstjs7440@naver.com>
2022-08-04 16:51:06 +0900

    가위바위보 진행중

--- src/pages/rsp/RSP.jsx
+++ src/pages/rsp/RSP.jsx
@@ -8,55 +8,71 @@ const rspCoords = {
 };
 
 const scores = {
-  가위: 1,
   바위: 0,
+  가위: 1,
   보: -1,
 };
 
+const computerChoice = (coord) => {
+  if (coord === "0") {
+    return "바위";
+  } else if (coord === "-142px") {
+    return "가위";
+  } else if (coord === "-284px") {
+    return "보";
+  }
+};
+
 function RSP() {
-  const [imgCoord, setImgCoord] = React.useState(0);
+  const [imgCoord, setImgCoord] = React.useState("0");
   const [result, setResult] = React.useState("");
   const [score, setScore] = React.useState(0);
   const interval = React.useRef(null);
 
-  React.useCallback(() => {
-    console.log("useCallback");
-    //     interval.current = setInterval(() => {
-    //       console.log("setInterval");
-    //       if (imgCoord === rspCoords.바위) {
-    //         console.log("바위");
-    //         setImgCoord(rspCoords.가위);
-    //       } else if (imgCoord === rspCoords.가위) {
-    //         console.log("보");
-    //         setImgCoord(rspCoords.보);
-    //       } else if (imgCoord === rspCoords.보) {
-    //         console.log("가위");
-    //         setImgCoord(rspCoords.바위);
-    //       }
-    //       console.log("test");
-    //     }, 1000);
+  React.useEffect(() => {
+    interval.current = setInterval(() => {
+      console.log("imgCoord: ", imgCoord);
+      if (imgCoord === rspCoords.바위) {
+        setImgCoord(rspCoords.가위);
+      } else if (imgCoord === rspCoords.가위) {
+        setImgCoord(rspCoords.보);
+      } else if (imgCoord === rspCoords.보) {
+        setImgCoord(rspCoords.바위);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval.current);
+    };
   }, [interval.current]);
 
-  console.log("바깥");
-  const onClickMe = (e) => {
+  const onClickMe = (choice) => {
     clearInterval(interval.current);
-    // if (e.target.textContent === "가위") {
-    //   setMe("가위");
-    // } else if (e.target.textContent === "바위") {
-    //   setMe("바위");
-    // } else if (e.target.textContent === "보") {
-    //   setMe("보");
-    // }
-    // console.log(me);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+
+    if (diff === 0) {
+      setResult("비겼습니다!!");
+    } else if ([-1, 2].includes(diff)) {
+      setResult("이겼습니다!!");
+      setScore((prev) => prev + 1);
+    } else {
+      setResult("졌습니다!");
+      setScore((prev) => prev - 1);
+    }
+
+    console.log("myScore: ", myScore);
+    console.log("cpuScore: ", cpuScore);
   };
 
   return (
     <>
       <ComputerStyle style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
-      <button onClick={onClickMe}>가위</button>
-      <button onClick={onClickMe}>바위</button>
-      <button onClick={onClickMe}>보</button>
-      <div>나 : {}</div>
+      <button onClick={() => onClickMe("가위")}>가위</button>
+      <button onClick={() => onClickMe("바위")}>바위</button>
+      <button onClick={() => onClickMe("보")}>보</button>
+      <div>나 : {result}</div>
+      <div>점수 : {score}</div>
     </>
   );
 }
