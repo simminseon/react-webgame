5a56758b40f6d10ba7c28cdc0ae0381b0ae7a66a
simminseon <alstjs7440@naver.com>
2022-08-26 15:10:12 +0900

    틱택토 구현중

--- src/pages/tictactoe/Tictactoe.js
+++ src/pages/tictactoe/Tictactoe.js
@@ -5,47 +5,69 @@ import Square from "./Square";
 
 const initialData = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function Tictactoe() {
+  // React.useEffect(() => {
+
+  // }, [boardData])
+
   const [boardData, setBoardData] = React.useState(initialData);
   const [turn, setTurn] = React.useState(1);
-  const [check, SetCheck] = React.useState([]);
+  const [result, setResult] = React.useState("");
+  const [count, setCount] = React.useState(0);
+  const [history, setHistory] = React.useState([]);
+  const winner = calculateWinner(boardData);
 
-  const onClickSqure = (selectedId) => {
-    if (turn === 1) {
-      setTurn(2);
-    } else if (turn === 2) {
-      setTurn(1);
-    }
-
-    const selectedItem = boardData.map((data, index) => {
-      if (parseInt(selectedId) === index) {
-        return turn;
-      } else {
-        return data;
+  const onClickSqure = React.useCallback(
+    (selectedId) => {
+      if (turn === 1) {
+        setTurn(2);
+      } else if (turn === 2) {
+        setTurn(1);
       }
-    });
 
-    setBoardData(selectedItem);
+      const selectedItem = boardData.map((data, index) => {
+        if (parseInt(selectedId) === index) {
+          return turn;
+        } else {
+          return data;
+        }
+      });
 
-    console.log(boardData);
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+      setBoardData(selectedItem);
+      setHistory((prev) => [...prev, boardData]);
+      setCount((prev) => prev + 1);
+      console.log(boardData);
+      console.log(history);
+      if (winner) {
+        const whoIsWinner = winner === 1 ? "O" : "x";
+        setResult(`${whoIsWinner}가 이겼습니다.`);
+      }
+    },
+    [boardData, winner, turn]
+  );
 
-    const test = [];
-    let findO = boardData.indexOf(1);
-    while (findO !== -1) {
-      test.push(findO);
-      findO = boardData.indexOf(1, findO + 1);
-    }
-    console.log(test);
+  const onClickHistory = (data) => {
+    setBoardData(data);
   };
   return (
     <>
@@ -63,6 +85,15 @@ function Tictactoe() {
           );
         })}
       </BoardStyle>
+      {history.map((data, index) => {
+        return (
+          <button type="button" onClick={() => onClickHistory(data)}>
+            {index}번째로 돌아가기
+          </button>
+        );
+      })}
+      <div>{count}</div>
+      <div>{result}</div>
     </>
   );
 }
