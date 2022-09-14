f29ec5e4ce9952f3d1e301be41ef1e8a894c71a0
simminseon <alstjs7440@naver.com>
2022-09-14 09:17:19 +0900

    화면 전환 state변경, 컬러 추가

--- src/pages/rpg/RPG.js
+++ src/pages/rpg/RPG.js
@@ -12,16 +12,21 @@ const monsterList = [
   { name: "마왕", hp: 150, att: 35, xp: 50 },
 ];
 
+const colorSet = {
+  hp: "green",
+  xp: "blue",
+  att: "orange",
+};
+
 function RPG() {
-  const [start, setStart] = useState(true);
-  const [game, setGame] = useState(false);
-  const [battle, setBattle] = useState(false);
+  const [screen, setScreen] = useState("start");
   const [value, onChangeValue, reset] = useInput("");
   const [monster, setMonster] = useState(null);
   const [hero, setHero] = useState(null);
   const [message, setMessage] = useState("");
 
   const inputRef = useRef(null);
+  const inputRef2 = useRef(null);
   class Character {
     constructor(name, hp, xp, att) {
       this.name = name;
@@ -60,8 +65,12 @@ function RPG() {
   }
 
   const onClickStart = () => {
-    setGame(true);
-    setStart(false);
+    if (value === "") {
+      setMessage("한글자 이상 입력하세요");
+      inputRef.current.focus();
+      return;
+    }
+    setScreen("game");
     reset("");
     setMessage("");
     const hero = new Hero(value);
@@ -84,16 +93,14 @@ function RPG() {
       setMonster(monster);
 
       setMessage(`몬스터를 만났다! ${monster.name}인 것 같다!`);
-      setBattle(true);
-      setGame(false);
+      setScreen("battle");
     } else if (value === "2") {
       // 휴식
       hero.hp = hero.maxHp;
       setMessage("충분한 휴식을 취했다.");
     } else if (value === "3") {
       // 종료
-      setGame(false);
-      setStart(true);
+      setScreen("start");
       setMessage("");
       reset("");
     }
@@ -101,7 +108,7 @@ function RPG() {
 
   const onClickBattle = () => {
     reset("");
-    inputRef.current.focus();
+    inputRef2.current.focus();
 
     if (value === "1") {
       hero.attack(monster);
@@ -111,14 +118,12 @@ function RPG() {
         setMessage(`${hero.lev} 레벨에서 전사. 새 주인공을 생성하세요.`);
         setHero(null);
         setMonster(null);
-        setStart(true);
-        setBattle(false);
+        setScreen("start");
       } else if (monster.hp <= 0) {
         setMessage(`몬스터를 잡아 ${monster.xp} 경험치를 얻었다.`);
         hero.getXp(monster.xp);
         setMonster(null);
-        setGame(true);
-        setBattle(false);
+        setScreen("game");
       } else {
         setMessage(
           `${hero.att}의 데미지를 주고 ${monster.att}의 데미지를 받았다`
@@ -135,13 +140,11 @@ function RPG() {
         setMessage(`${hero.lev} 레벨에서 전사. 새 주인공을 생성하세요.`);
         setHero(null);
         setMonster(null);
-        setStart(true);
-        setBattle(false);
+        setScreen("start");
       }
     } else if (value === "3") {
       // 도망
-      setGame(true);
-      setBattle(false);
+      setScreen("game");
       setMessage("부리나케 도망쳤다!");
       setMonster(null);
     }
@@ -150,11 +153,11 @@ function RPG() {
   return (
     <>
       <Title>RPG게임</Title>
-      {start && (
+      {screen === "start" && (
         <>
           <span>주인공을 입력하세요!</span>
           <BoxStyle>
-            <Input onChange={onChangeValue} value={value} />
+            <Input onChange={onChangeValue} value={value} ref={inputRef} />
             <Button type="button" onClick={onClickStart}>
               시작
             </Button>
@@ -162,22 +165,22 @@ function RPG() {
         </>
       )}
       <div id="screen">
-        {(game || battle) && (
+        {(screen === "game" || screen === "battle") && (
           <div id="hero-stat">
             <StatStyle bold>{hero.name}</StatStyle>
             <StatStyle>(Lev {hero.lev}) </StatStyle>
 
-            <StatStyle color={"red"}>
+            <StatStyle color={colorSet.hp}>
               hp : {hero.hp}/{hero.maxHp}
             </StatStyle>
-            <StatStyle color={"blue"}>
+            <StatStyle color={colorSet.xp}>
               xp : {hero.xp}/{15 * hero.lev}
             </StatStyle>
-            <StatStyle color={"orange"}>att : {hero.att}</StatStyle>
+            <StatStyle color={colorSet.att}>att : {hero.att}</StatStyle>
           </div>
         )}
-        {game && (
-          <GameBox>
+        {screen === "game" && (
+          <GameBoxStyle>
             <div id="menu-1">1.모험</div>
             <div id="menu-2">2.휴식</div>
             <div id="menu-3">3.종료</div>
@@ -187,30 +190,30 @@ function RPG() {
                 입력
               </Button>
             </BoxStyle>
-          </GameBox>
+          </GameBoxStyle>
         )}
-        {battle && (
-          <GameBox>
+        {screen === "battle" && (
+          <GameBoxStyle>
             <div id="battle-1">1.공격</div>
             <div id="battle-2">2.회복</div>
             <div id="battle-3">3.도망</div>
             <BoxStyle>
-              <Input onChange={onChangeValue} value={value} ref={inputRef} />
+              <Input onChange={onChangeValue} value={value} ref={inputRef2} />
               <Button type="button" onClick={onClickBattle}>
                 입력
               </Button>
             </BoxStyle>
-          </GameBox>
+          </GameBoxStyle>
         )}
-        <div id="message">{message}</div>
+        <MessageStyle>{message}</MessageStyle>
         {monster && (
           <div id="monster-stat">
             <StatStyle bold>{monster.name} </StatStyle>
-            <StatStyle color={"red"}>
+            <StatStyle color={colorSet.hp}>
               hp : {monster.hp}/{monster.maxHp}
             </StatStyle>
-            <StatStyle color={"blue"}>xp : {monster.xp}</StatStyle>
-            <StatStyle color={"orange"}>att : {monster.att}</StatStyle>
+            <StatStyle color={colorSet.xp}>xp : {monster.xp}</StatStyle>
+            <StatStyle color={colorSet.att}>att : {monster.att}</StatStyle>
           </div>
         )}
       </div>
@@ -227,7 +230,7 @@ const BoxStyle = styled.div`
   }
 `;
 
-const GameBox = styled.div`
+const GameBoxStyle = styled.div`
   padding: 10px 0;
 `;
 
@@ -238,6 +241,11 @@ const StatStyle = styled.span`
   padding-right: 10px;
   font-weight: ${(props) => (props.bold ? "bold" : "normal")};
   color: ${(props) => props.color || "black"};
+`;
+
+const MessageStyle = styled.div`
+  padding-bottom: 10px;
+  color: red;
 `;
 
 export default RPG;
